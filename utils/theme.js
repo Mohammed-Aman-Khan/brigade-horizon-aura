231e8aa2cd8b4d876fe6a47ba88090cb43825921
Mohammed Aman Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2023-11-12 19:43:54 +0530

    Update theme.js

--- utils/theme.js
+++ utils/theme.js
@@ -1,9 +1,9 @@
 import { createTheme } from "@mui/material";
-import { cyan } from "@mui/material/colors";
+import { blue } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
-    primary: cyan,
+    primary: blue,
   },
   typography: {
     button: {
